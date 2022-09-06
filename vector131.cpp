dbd874051f246a6578c6b803aea8d65a7e60599f
KingOfNothing16 <54426980+KingOfNothing16@users.noreply.github.com>
2022-09-05 23:18:31 -0700

    Update vector131.cpp

--- vector131.cpp
+++ vector131.cpp
@@ -5,32 +5,38 @@ using namespace std;
 
 vector131::vector131()
 {
+  // Variables 
   int count;
   int currentValue;
   int frontValue;
   int resizeVector;
-
+  
+  // Takes in user input to determine size of vector
   cout<<"Input Dynamic Array Size\n";
   cin >> count;
-
+  
+  // initialize vector
   std::vector<int> myVec;
-
+  
+  // Takes in user input to store in vector that loops by user initial count
   cout << "Input Values" << endl;
   for (int k = 0; k < count; k++)
   {
     
     cin >> currentValue;
+    // uses pushback member function to insert values
     myVec.push_back(currentValue);
 
   }
-
+  // for loop to print each value in vector
   for (int i: myVec)
   {
     cout << i << ", ";
   }
 
   cout << endl;
-
+  
+  // Asks user to resize the array to but must be smaller than initial value
   cout << "Which value would you like to resize the array to?";
   cin >> resizeVector;
   while (count < resizeVector)
@@ -39,6 +45,7 @@ vector131::vector131()
       cout << "Which value would you like to resize the array to?";
       cin >>  resizeVector;
   }
+  // resizes vector based on user input
   myVec.resize(resizeVector);
   for (int i: myVec)
   {
@@ -46,9 +53,12 @@ vector131::vector131()
   }
 
   cout << endl;
-
+  
+  // Asks user to choose a value to insert into vector from the front
   cout << "What value would you like to add to the front?: ";
   cin >> frontValue;
+  
+  // push front would not work so I used insert instead
   myVec.insert(myVec.begin(),frontValue);
 
   for (int i: myVec)
